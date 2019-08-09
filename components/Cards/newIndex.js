754334153976879db3e9e7222bca4bba9efb2c44
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-08-09 13:21:53 -0400

    added axios and did a for loop on data to create article cards, created functional component variables added classes and appended them

--- components/Cards/newIndex.js
+++ components/Cards/newIndex.js
@@ -18,25 +18,51 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
-axios 
-.get(
-.then(response => {
-    
-})
-)
-.catch(error => {
-    console.log("Error", error)
-})
+const cards = document.querySelector(".cards-container");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log("article response", response.data);
+    const articles = response.data.articles;
+    for (topic in articles) {
+      aticles[topic].forEach(item => {
+        cards.apprndChild(newArticle(item));
+      });
+    }
+  })
+
+  .catch(error => {
+    console.log("Error", error);
+  });
 
 function newArticle(item) {
+  //create dom variables
+  const card = document.createElement("div");
+  const headline = document.createElement("div");
+  const author = document.createElement("div");
+  const imgContainer = document.createElement("div");
+  const img = document.createElement("img");
+  const authorName = document.createElement("span");
 
-    //create dom variables
+  //set styles
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
 
-    //set styles
+  //text to content
+  headline.textContent = `${item.headline}`;
+  author.textContent = `Author: ${item.authorName}`;
+  img.src = `${item.img}`;
 
-    //text to content
+  //append child
+  cards.appendChild(card);
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  imgContainer.appendChild(img);
+  author.appendChild(authorName);
 
-    //append child
-
-    return card
+  return card;
 }
