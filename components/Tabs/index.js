b27eb3a9a0a771890f2d48ba942c886cbb5a0399
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-08-09 11:24:35 -0400

    made new files

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,37 +8,33 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
-
-
-const topics = document.querySelector('.topics')
+const topics = document.querySelector(".topics");
 
 axios
-    .get('https://lambda-times-backend.herokuapp.com/topics')
-    .then(response => {
-        //here is where we get results from server
-        console.log('response', response)
-        response.data.topics.forEach(articleTopic => {
-            topics.appendChild(createTab(articleTopic))
-        })
-    })
-    .catch((error) => {
-        console.log('ERROR', error)
-    })
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    //here is where we get results from server
+    console.log("response", response);
+    response.data.topics.forEach(articleTopic => {
+      topics.appendChild(createTab(articleTopic));
+    });
+  })
+  .catch(error => {
+    console.log("ERROR", error);
+  });
 
 function createTab(topicObject) {
+  //create elements
+  const tab = document.createElement("div");
 
-    //create elements
-    const tab = document.createElement('div')
+  //set styles
+  tab.classList.add("tab");
 
-    //set styles
-    tab.classList.add('tab')
+  //append to variables
+  tab.appendChild(topics);
 
-    //append to variables
-    // tab.appendChild(topics)
+  //text to content
+  tab.textContent = topicObject;
 
-
-    //text to content
-    tab.textContent = topicObject
-
-    return tab
+  return tab;
 }
